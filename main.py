47bfeb7c6d46c50367bf17ac178154112c3db031
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-04-13 12:57:50 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# main.py
+
+from telegram.ext import Updater
+from config import BOT_TOKEN
+from handlers.start_handler import start_handler
+from handlers.query_handler import query_handler
+from handlers.force_join import force_join_handler
+
+def main():
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    # Handlers
+    dp.add_handler(start_handler)
+    dp.add_handler(force_join_handler)
+    dp.add_handler(query_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
