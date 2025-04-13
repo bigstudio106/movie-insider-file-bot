36f3697c7adb54e53ef57dcbafa44e00ef803eda
bigstudio106 <bigstudiomrjaat@gmail.com>
2025-04-14 01:04:11 +0530

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,12 +1,11 @@
-from config import LOG_GROUP, API_ID, API_HASH, BOT_TOKEN
-from pyrogram import Client
+from config import LOG_GROUP, BOT_TOKEN
+from telegram import Bot
 
-# Pyrogram client for logging
-log_client = Client("logger_session", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 
-async def send_log(message: str):
+def send_log(message: str):
     if LOG_GROUP:
         try:
-            await log_client.send_message(chat_id=int(LOG_GROUP), text=message)
+            bot.send_message(chat_id=int(LOG_GROUP), text=message)
         except Exception as e:
             print(f"[LOG ERROR]: {e}")
